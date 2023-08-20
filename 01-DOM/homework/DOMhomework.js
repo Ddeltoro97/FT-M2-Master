095debfd99119e472117dbb365e281ad82692e5f
Ddeltoro97 <davison09.ddt@gmail.com>
2023-08-19 19:25:29 -0500

    hw actualizadas

--- 01-DOM/homework/DOMhomework.js
+++ 01-DOM/homework/DOMhomework.js
@@ -7,7 +7,9 @@ let toDoItems = [];
 // Usando querySelector seleccionar dicho span por su id ('createdBy') y luego usando innerHTML
 // agregar tu nombre al final del texto actual. Ej: 'Aplicación creada por Franco'
 // Tu código acá:
-
+let span = document.querySelector('#createdBy'); //document engobla todo lo del DOM
+                                                 //querySelector es un método 
+span.innerHTML += " David Del Toro"; //dejarte acceder o modificar al contenido
 // Crear una clase denominada 'ToDo' cuyo constructor debe recibir un único parámetro del tipo string
 // con el nombre 'description' que será justamente la descripción del ToDo.
 // Agregar dos propiedades a la clase:
@@ -17,6 +19,8 @@ let toDoItems = [];
 
 function ToDo(description) {
   // Tu código acá:
+  this.description = description;
+  this.complete = false;
 }
 
 // Agregar un método denominado 'completeToDo' al prototipo de la clase ToDo
@@ -24,7 +28,9 @@ function ToDo(description) {
 // Debe setear el atributo 'complete' del ToDo en true
 
 // Tu código acá:
-
+ToDo.prototype.completeToDo = function(){
+  this.complete = true;
+}
 // Agregar dos parámetros a la función 'buildToDo':
 //    1) Un objeto de la clase ToDo
 //    2) Index numérico
@@ -44,6 +50,18 @@ function ToDo(description) {
 
 function buildToDo(todo, index) {
   // Tu código acá:
+  let toDoShell = document.createElement("div");
+  toDoShell.className = "toDoShell";
+  let toDoText = document.createElement("span");
+  toDoText.innerHTML = todo.description;
+  toDoText.id = index;
+  if(todo.complete == true){
+    toDoText.className = "completeText";
+  }
+  toDoShell.appendChild(toDoText);
+  toDoText.addEventListener('click', completeToDo);
+  return toDoShell;
+
 }
 
 // La función 'buildToDos' debe crear un array de objetos toDo y devolverlo
@@ -53,6 +71,11 @@ function buildToDo(todo, index) {
 
 function buildToDos(toDos) {
   // Tu código acá:
+  let array = toDos.map(function (todo, index){
+    return buildToDo(todo, index);
+  })
+return array;
+
 }
 
 // La función 'displayToDos' se va a encargar de que se vean los toDo's en pantalla
@@ -66,6 +89,12 @@ function buildToDos(toDos) {
 
 function displayToDos() {
   // Tu código acá:
+  let toDoContainer = document.querySelector("#toDoContainer");
+  toDoContainer.innerHTML = "";
+  let punto4 = buildToDos(toDoItems);
+  for(let i = 0; i < punto4.length; i++){
+    toDoContainer.appendChild(punto4[i]);
+  }
 }
 
 // La función 'addToDo' agregará un nuevo ToDo al array 'toDoItems'
@@ -79,6 +108,11 @@ function displayToDos() {
 
 function addToDo() {
   // Tu código acá:
+  let input = document.querySelector("#ToDoInput");
+  let TuDu = new ToDo(input.value);
+  toDoItems.push(TuDu);
+  input.value = "";
+  displayToDos();
 }
 
 // Agregar un 'Event Listener' para que cada vez que el botón 'AGREGAR' sea clickeado
@@ -87,6 +121,8 @@ function addToDo() {
 //   2) Agregarle un 'click' event listener, pasándole la función 'addToDo' como callback
 
 // Tu código acá:
+let variable = document.querySelector("#addButton");
+variable.addEventListener('click', addToDo);
 
 // La función completeToDo se va a ejecutar cuando queramos completar un todo
 // [NOTA: Algunas cuestiones a tener en cuenta
@@ -102,8 +138,11 @@ function addToDo() {
 
 function completeToDo(event) {
   // DESCOMENTAR LA SIGUIENTE LINEA
-  //const index = event.target.id;
-  // Tu código acá:
+   //const index = event.target.id;
+   const index = event.target.id;
+   // Tu código acá:
+   toDoItems[index].completeToDo;
+   displayToDos();
 }
 
 // Una vez que llegaste a este punto verificá que todos los tests pasen
--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -9,6 +9,15 @@ var traverseDomAndCollectElements = function (matchFunc, startEl) {
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
+  if(matchFunctionMaker(startEl)) resultSet.push(startEl);
+
+  for (let i = 0; i < startEl.children.length; i++){ //recorrer todos los hijos de startEl
+    let aux = traverseDomAndCollectElements(matchFunctionMaker,startEl.children[i]); //recoge los hijos
+    resultSet = [...resultSet, ...aux]; //con esto concatenamos ambos arrays.
+  }
+
+  return resultSet;
+
 };
 
 // Detecta y devuelve el tipo de selector
@@ -16,6 +25,10 @@ var traverseDomAndCollectElements = function (matchFunc, startEl) {
 
 var selectorTypeMatcher = function (selector) {
   // tu código aquí
+  if (selector[0] === "#") return "id"; //los ID comienzan con #
+  if (selector[0] === ".") return "class"; //las clases comienzan con .
+  if (selector.split(".").length > 1) return "tag.class"; //dividimos la palabra y si el punto no está al comienzo significa que es un tag class
+  return "tag"; //Si no se cumple ninguno, retornamos tag
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -23,13 +36,53 @@ var selectorTypeMatcher = function (selector) {
 // parametro y devuelve true/false dependiendo si el elemento
 // matchea el selector.
 
-var matchFunctionMaker = function (selector) {
-  var selectorType = selectorTypeMatcher(selector);
-  var matchFunction;
+let matchFunctionMaker = function (selector) { //"#hola", ".class", "div", "span.hola"
+  let selectorType = selectorTypeMatcher(selector);
+  let matchFunction;
   if (selectorType === "id") {
+
+    //Lo que hace esta función es comparar el element con el selector, si son iguales retorna true y si no es asi retorna false
+    matchFunction = (element) => `#${element.id}` === selector //el elemento es una etiqueta de HTML
+    //Lo que hicimos fue definir la variable (es una función).
+    //Usamos element.id para acceder a la propiedad del elemento ya que se recibe como un objeto
+    //le agregamos el # porque el selector viene con el pero el elemento no
+
   } else if (selectorType === "class") {
+
+    matchFunction = (element) => {
+      let classes = element.classList; //esto es un método que te da un array con todas las clases del element
+
+      return classes.contains(selector.slice(1)); //Una mejor forma de hacerlo
+      //Acá usamos slice desde la posición 1 para no incluir el punto de selector a la hora de comparar
+
+
+    //   for(let i = 0; i < classes.length; i++){
+    //     if(`.${classes[i]}` === selector) return true; //Busca el selector entre las clases del elemento
+    //   }
+    //     return false; //Si no encuentra coincidencias retorna false
+  } 
+
   } else if (selectorType === "tag.class") {
+
+    matchFunction = (element) => {
+      const [tag, className] =selector.split('.') //Separamos el string en un array con las dos palabras. Separamos con el .
+
+
+      //Tenemos que aplicar esta recursión para ambos métodos ya que son de tipo distinto
+      return matchFunctionMaker(tag)(element) && matchFunctionMaker(`.${className}`)(element);
+      //ponemos el (element para llamar la otra función)
+      //eso es como decir: let match = matchFunctionMaker(tag)
+                         //match(element)
+    }
+
+
   } else if (selectorType === "tag") {
+
+   //tagname es una propiedad que te permite acceder al elemento HTML
+   //lo devuelve en string y en mayúsculas, por eso es necesario transformar selector a mayúscula
+    matchFunction = (element) => element.tagName === selector.toUpperCase();
+
+
   }
   return matchFunction;
 };
